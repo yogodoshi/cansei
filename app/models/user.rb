d943c8706104cde4c4cb584a24eb9ccef16412bb
Cayo Medeiros (yogodoshi) <yogodoshi@gmail.com>
2017-10-15 03:20:33 +0700

    Implement User.find_or_create_from_auth_hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
 
   validates :provider, :uid, :username, :token, :secret, :profile_image, :frequency, presence: true
   validates :uid, uniqueness: { scope: :provider }
+
+  def self.find_or_create_from_auth_hash(auth_hash)
+    user = where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create
+    user.update(
+      username: auth_hash.info.nickname,
+      profile_image: auth_hash.info.image,
+      token: auth_hash.credentials.token,
+      secret: auth_hash.credentials.secret
+    )
+    user
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,52 @@ RSpec.describe User, type: :model do
 
     it { should validate_uniqueness_of(:uid).scoped_to(:provider) }
   end
+
+  describe '.find_or_create_from_auth_hash' do
+    subject { User.find_or_create_from_auth_hash(OmniAuth.config.mock_auth[:twitter]) }
+
+    context 'when the User doesnt exist in the database' do
+      let(:created_user) { User.last }
+
+      it 'creates the User with correct attributes' do
+        expect do
+          subject
+        end.to change { User.count }.by(1)
+
+        expect(created_user.provider).to eq('twitter')
+        expect(created_user.uid).to eq('bathjJwvdhKjgfgh8Jd745J7dh5Qkgflbnczd65dfnw')
+        expect(created_user.token).to eq('1234')
+        expect(created_user.secret).to eq('5678')
+        expect(created_user.username).to eq('yogodoshi')
+        expect(created_user.profile_image).to eq('https://pbs.twimg.com/profile_images/849013196325302277/KlcHE1l1_400x400.jpg')
+      end
+
+      it 'returns the created user' do
+        expect(subject).to eq(created_user)
+      end
+    end
+
+    context 'when the User already exists in the database' do
+      let!(:user) { create(:user, :from_mocked_twitter_auth, profile_image: 'http://another_url.com/avatar.jpg', username: 'not_yogodoshi') }
+
+      it 'updates the User with given attributes' do
+        expect do
+          subject
+        end.to_not change { User.count }
+
+        user.reload
+
+        expect(user.provider).to eq('twitter')
+        expect(user.uid).to eq('bathjJwvdhKjgfgh8Jd745J7dh5Qkgflbnczd65dfnw')
+        expect(user.token).to eq('1234')
+        expect(user.secret).to eq('5678')
+        expect(user.username).to eq('yogodoshi')
+        expect(user.profile_image).to eq('https://pbs.twimg.com/profile_images/849013196325302277/KlcHE1l1_400x400.jpg')
+      end
+
+      it 'returns the found user' do
+        expect(subject).to eq(user)
+      end
+    end
+  end
 end
