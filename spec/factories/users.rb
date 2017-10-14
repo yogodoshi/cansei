36cf81c101dcfed460bf46a49a4e6e23e08f2d0f
Cayo Medeiros (yogodoshi) <yogodoshi@gmail.com>
2017-10-15 03:18:42 +0700

    Add from_mocked_twitter_auth trait to User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,5 +9,14 @@ FactoryGirl.define do
     secret '5678EfGh'
     profile_image 'https://pbs.twimg.com/profile_images/849013196325302277/KlcHE1l1_400x400.jpg'
     frequency 0
+
+    trait :from_mocked_twitter_auth do
+      provider 'twitter'
+      uid 'bathjJwvdhKjgfgh8Jd745J7dh5Qkgflbnczd65dfnw'
+      token '1234'
+      secret '5678'
+      username 'yogodoshi'
+      profile_image 'https://pbs.twimg.com/profile_images/849013196325302277/KlcHE1l1_400x400.jpg'
+    end
   end
 end
